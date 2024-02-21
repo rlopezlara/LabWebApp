e4f9b68b438f95f8fc9ec1d23a64346eccd74852
rlopezlara <200549271@student.georgianc.on.ca>
2024-02-21 13:25:03 -0500

    Add project files.

---
+++ LabWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using LabWebApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product>Products { get; set; }
+
+    }
+}
---
+++ LabWebApp/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabWebApp.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [Display(Name = "First Name")]
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+       
+    }
+
+}
